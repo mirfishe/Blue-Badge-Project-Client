394920622e03f953bde4d45e830c383f39e9181e
Michael Fisher <mirfishe@gmail.com>
2020-09-15 15:42:50 -0400

    fixing issues with login, logout and register buttons displaying at wrong times

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import {Container, Col, Row} from "reactstrap";
+import {Container, Col, Row, Nav, NavbarBrand, NavbarText} from "reactstrap";
 import Header from "./components/site/Header";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Logout from "./components/auth/Logout";
 import Footer from "./components/site/Footer";
 import Search from "./components/search/Search";
 import List from "./components/list/List";
@@ -39,10 +42,33 @@ function App() {
     console.log("User logged out.");
   };
 
+  // Didn't work to re-render the Header.
+  // useEffect(() => {
+  //   displayHeader();
+  // }, [sessionToken]);
+
+  // Didn't work to re-render the Header.
+  // const displayHeader = () => {
+  //   return (
+  //     <Row>
+  //     <Header baseURL={baseURL} sessionToken={sessionToken} updateToken={updateToken} clearToken={clearToken} />
+  //   </Row>
+  //   )};
+
   return (
     <Container>
+      {/* displayHeader() */ /* Didn't work to re-render the Header. */}
       <Row>
-        <Header baseURL={baseURL} sessionToken={sessionToken} updateToken={updateToken} clearToken={clearToken} />
+      {/* <Header baseURL={baseURL} sessionToken={sessionToken} updateToken={updateToken} clearToken={clearToken} /> */}
+        <Nav>
+            <NavbarBrand>Logo</NavbarBrand>
+            {/* {sessionToken !== localStorage.getItem("token") ? <Login baseURL={baseURL} updateToken={updateToken} /> : ""}
+            {sessionToken !== localStorage.getItem("token") ? <Register baseURL={baseURL} updateToken={updateToken} /> : ""}
+            {sessionToken === localStorage.getItem("token") && sessionToken !== undefined ? <Logout clearToken={clearToken} /> : ""} */}
+            {localStorage.getItem("token") === null || localStorage.getItem("token") === undefined  ? <Login baseURL={baseURL} updateToken={updateToken} />  : ""}
+            {localStorage.getItem("token") === null || localStorage.getItem("token") === undefined  ? <Register baseURL={baseURL} updateToken={updateToken} /> : ""}
+            {localStorage.getItem("token") !== null && localStorage.getItem("token") !== undefined  ? <Logout clearToken={clearToken} />  : ""}
+        </Nav>
       </Row>
       <Row className="mp-3">
       <Col xs="4">
