b8f277a104ab0284655b2ff75a3360003c7bcfcd
Michael Fisher <mirfishe@gmail.com>
2020-09-15 13:56:50 -0400

    adding environment.js

---
+++ src/helpers/environment.js
@@ -0,0 +1,12 @@
+let API_URL = "";
+
+switch (window.location.hostname) {
+    case "localhost" || "127.0.0.1":
+        API_URL = "http://localhost:" + process.env.REACT_APP_SERVER_PORT;
+        break;
+    case "tw-blue-badge-server":
+        API_URL = "https://tw-blue-badge-server";
+        break;
+};
+
+export default API_URL;
