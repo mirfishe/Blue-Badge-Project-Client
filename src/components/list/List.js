98806c487e6f8830d0dc269ad05eefe491cf9026
Michael Fisher <mirfishe@gmail.com>
2020-09-14 20:32:45 -0400

    added sub components

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,12 +1,41 @@
 import React, {useState} from "react";
-import {Container, Col, Row} from "reactstrap";
+import {Container, Col, Row, Nav, NavItem, NavLink, TabContent, TabPane} from "reactstrap";
+import classnames from 'classnames';
+import ListItems from "./ListItems";
 
 const List = () => {
 
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+      if(activeTab !== tab) setActiveTab(tab);
+    };
+
     return (
-        <Row>
-            <Col>List</Col>
-        </Row>
+        <div>
+        <Nav tabs>
+            <NavItem>
+                <NavLink className={classnames({ active: activeTab === '1' })} onClick={() => { toggle('1'); }}>
+                    My List
+                </NavLink>
+            </NavItem>
+        </Nav>
+        <Nav tabs>
+            <NavItem>
+            <NavLink className={classnames({ active: activeTab === '2' })} onClick={() => { toggle('2'); }}>
+                    My List
+                </NavLink>
+            </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+        <ListItems />
+        </TabPane>
+        <TabPane tabId="2">
+        <ListItems />
+        </TabPane>
+      </TabContent>
+        </div>
     );
 };
 
---
+++ src/components/search/Results.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import {Container, Col, Row} from "reactstrap";
+
+const Results = () => {
+
+    return (
+        <Row>
+            <Col>Search Results</Col>
+        </Row>
+    );
+};
+
+export default Results;
